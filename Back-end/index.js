e7a70272c69fe478d8f56143bad34157002957c2
unknown <IT_AI_GWX@163.COM>
2015-10-08 10:10:16 +0800

    add template engine

--- Back-end/index.js
+++ Back-end/index.js
@@ -1,15 +1,21 @@
 'use strict'
+/* dependencies */
 let app = require('express')();
 let http = require('http');
+let path = require('path');
 
+/* configurations */
+app.set('PORT', process.env.PORT || 8000)
+app.set('view engine', 'jade');
+app.set('views', path.join(__dirname, 'views'));
+
+/* routers */
 app.get('/', (req, res) => {
   console.log('GET IT');
   res.end();
 });
 
-var server = http.createServer(app);
-server.listen(2000, () => {
-  
+let server = http.createServer(app);
+server.listen(app.get('PORT'), () => {
+  console.log(`Server is running now, listenning at ${app.get('PORT')}`);
 });
-//what render template use to set?
-// app.set('view engine', );
