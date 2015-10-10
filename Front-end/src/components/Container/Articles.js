930c43941c3a4badec1c82d84964a852c035e734
David <Ecutwxguan@163.com>
2015-10-10 22:12:11 +0800

    Add fake blogs.

--- Front-end/src/components/Container/Articles.js
+++ Front-end/src/components/Container/Articles.js
@@ -1,11 +1,23 @@
 import React from 'react/addons';
 import './Articles.scss';
+import {Card, CardHeader, CardText} from'material-ui';
+let tiltes = ['A', 'B', 'D', 'E', 'G', 'H'];
 
 export default class Articles extends React.Component{
   render(){
     return (
       <div className='Articles'>
-        <h1>Articles</h1>
+        {
+          tiltes.map(item => {
+            return (
+              <Card className='Card'>
+                <CardHeader />
+                <CardText>
+                  {`The content of blog post ${item}`}
+                </CardText>
+              </Card>);
+          })
+        }
       </div>
     );
   }
--- Front-end/src/components/Container/TechNotes.js
+++ Front-end/src/components/Container/TechNotes.js
@@ -1,11 +1,23 @@
 import React from 'react/addons';
 import './TechNotes.scss';
+import {Card, CardHeader, CardText} from'material-ui';
+let tiltes = ['K', 'B', 'D', 'E', 'G', 'H'];
 
 export default class TechNotes extends React.Component{
   render(){
     return (
       <div className='TechNotes'>
-        <h1>TechNotes</h1>
+        {
+          tiltes.map(item => {
+            return (
+              <Card className='Card'>
+                <CardHeader />
+                <CardText>
+                  {`The content of tech blog post ${item}`}
+                </CardText>
+              </Card>);
+          })
+        }
       </div>
     );
   }
