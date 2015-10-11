040cfcd3ec2c1bd0655823b80e1da3430c28a500
David <Ecutwxguan@163.com>
2015-10-11 22:50:07 +0800

    Add basic "me" info

--- Front-end/src/components/Container/Me.js
+++ Front-end/src/components/Container/Me.js
@@ -8,20 +8,82 @@ export default class Me extends React.Component{
   render(){
     return (
       <div className='Me'>
+        <h1 className='title'>
+          Hi everyone, I{"'"}m David Guan.
+          <br />
+          Coder, Blog writer, Books lover.
+        </h1>
+
+        <div className='ContentRow'>
+          <FontIcon style={{fontSize: '6em', color: '#00bcd4'}}
+            className='fa fa-graduation-cap icon left'
+            hoverColor='#E74748'/>
+          <article className='content'>
+            My
+            <a href='http://ecutdavid.github.io/WenxiangGuanResume.github.io/'>first resume</a>
+            finished on Jun 2, 2015.
+            <br />
+            It summarize my campus life :)
+            <br />
+          </article>
+        </div>
+
+        <div className='ContentRow'>
+          <article className='content left'>
+            Working at Newegg.com After Jun 13, 2015.
+            <br />
+            Based on ASP.Net
+          </article>
+          <FontIcon style={{fontSize: '6em', color: '#00bcd4'}}
+            className='fa fa-laptop icon right'
+            hoverColor='#E74748'/>
+        </div>
+
+        <div className='ContentRow'>
+          <FontIcon style={{fontSize: '6em', color: '#00bcd4'}}
+            className='fa fa-book icon left'
+            hoverColor='#E74748'/>
+          <article className='content'>
+            Every day after work,
+            <br />
+            I begin study and practise new techs.
+            <br />
+            never stop improve my skills and efficiency.
+          </article>
+        </div>
+
+        <div className='ContentRow'>
+          <article className='content left'>
+            <a href='https://github.com/EcutDavid'>Github profile</a>
+            <br />
+            build project on github repo everyday.
+            <br />
+            Enjoy Make contibution to open source.
+          </article>
+          <FontIcon style={{fontSize: '6em', color: '#00bcd4'}}
+            className='fa fa-github icon right'
+            hoverColor='#E74748'/>
+        </div>
+
         <div className='ContentRow'>
           <FontIcon style={{fontSize: '6em', color: '#00bcd4'}}
             className='fa fa-file-code-o icon left'
             hoverColor='#E74748'/>
           <article className='content'>
-            This is my first
+            I enjoy writing Blog,
+            <br />
+            But <a href='http://www.cnblogs.com/E-WALKER/'>this Blog</a> is in Chinese languages.
+            <br />
+            So I start build this site.
           </article>
         </div>
+
         <div className='ContentRow'>
           <article className='content left'>
-            This is my first
+            I will try my best to do every thing!
           </article>
           <FontIcon style={{fontSize: '6em', color: '#00bcd4'}}
-            className='fa fa-file-code-o icon right'
+            className='fa fa-heart-o icon right'
             hoverColor='#E74748'/>
         </div>
       </div>
