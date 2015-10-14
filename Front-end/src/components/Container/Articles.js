e5b41bc0cbf27a4e26c121868d1fc4d4894d9ad9
David <Ecutwxguan@163.com>
2015-10-14 20:10:59 +0800

    Front end add left link bar.

--- Front-end/src/components/Container/Articles.js
+++ Front-end/src/components/Container/Articles.js
@@ -1,19 +1,33 @@
 import React from 'react/addons';
 import './Articles.scss';
-import {Card, CardHeader, CardText} from'material-ui';
+import {Card, CardHeader, CardText, Avatar, LeftNav, MenuItem} from'material-ui';
 let tiltes = ['A', 'B', 'D', 'E', 'G', 'H'];
+
+let menuItems = [
+  { type: MenuItem.Types.SUBHEADER, text: 'Article type' },
+  { type: MenuItem.Types.SUBHEADER, text: 'Site Link' },
+  {
+     type: MenuItem.Types.LINK,
+     payload: '/',
+     text: 'Homepage'
+  }
+];
 
 export default class Articles extends React.Component{
   render(){
     return (
       <div className='Articles'>
+        <LeftNav style={{position: 'absolute', top: 48}} className='leftNav' ref='leftNav' menuItems={menuItems} />
         {
           tiltes.map(item => {
             return (
               <Card className='Card'>
-                <CardHeader />
+                <CardHeader
+                  title="Title"
+                  subtitle="Subtitle"
+                  avatar={<Avatar>JS</Avatar>}/>
                 <CardText>
-                  {`The content of blog post ${item}`}
+                  {`The content of blog pt ${item}`}
                 </CardText>
               </Card>);
           })
