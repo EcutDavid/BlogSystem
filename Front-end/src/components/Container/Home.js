7ebeb5a2988300ba138e8887531f8ae76b28863f
David <Ecutwxguan@163.com>
2015-10-26 13:14:44 +0800

    Adjust style for display in 340px width device.

--- Front-end/src/components/Container/Home.js
+++ Front-end/src/components/Container/Home.js
@@ -6,9 +6,7 @@ export default class Home extends React.Component{
     return (
       <div className='Home'>
         <h1>Welcome!</h1>
-        <h2>The site still on building.</h2>
-        <h2>If you have any suggestion, please tell me!</h2>
-        <h2>DavidGuanDev@Gmail.com</h2>
+        <p>DavidGuanDev@Gmail.com</p>
       </div>
     );
   }
